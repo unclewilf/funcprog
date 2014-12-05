07d1bc8442b25347d4a64ba5d247df5692c86ac9
Will Funnell <w.f.funnell@gmail.com>
2014-12-05 23:20:18 +0000

    Fib problem solved

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "funcprog"
 
 version := "1.0"
 
-scalaVersion := "2.11.0"
+scalaVersion := "2.11.4"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
 
--- src/main/scala/intro/Fib.scala
+++ src/main/scala/intro/Fib.scala
@@ -1,8 +1,24 @@
 package intro
 
-object Fib {
-  def fib(i: Int) : Int = {
-    0
-  }
+import scala.annotation.tailrec
 
+object Fib {
+
+  def fib(i: Int) : Int = {
+
+    @tailrec
+    def loop(i:Int, pos:Int, prev: Int, current: Int): Int = {
+      if (pos==i) {
+        current
+      } else {
+        loop(i, pos+1, current, prev+current)
+      }
+    }
+
+    if (i == 0) {
+      0
+    } else {
+      loop(i, 1, 0, 1)
+    }
+  }
 }
