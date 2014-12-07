43b8e0b22b8c7144db25ece80451eaf5fc637d6e
Will Funnell <w.f.funnell@gmail.com>
2014-12-07 19:33:30 +0000

    Depth added to tree

--- src/main/scala/datastructures/Tree.scala
+++ src/main/scala/datastructures/Tree.scala
@@ -10,13 +10,19 @@ object Tree {
   def size[A](a: Tree[A]): Int = a match {
 
     case Leaf(_) => 1
-    case Branch(l,r) => 1 + size(l) + size(r)
+    case Branch(l,r) => (size(l) + size(r)) + 1
   }
 
-  def maxInt(a: Tree[Int]): Int = a match {
+  def maximum(a: Tree[Int]): Int = a match {
 
     case Leaf(v) => v
-    case Branch(l,r) => maxInt(l).max(maxInt(r))
+    case Branch(l,r) => maximum(l) max maximum(r)
+  }
+
+  def depth(a: Tree[Int]): Int = a match {
+
+    case Leaf(v) => 0
+    case Branch(l,r) => (depth(l) max depth(r)) + 1
   }
 
 }
--- src/test/scala/datastructures/TreeSpec.scala
+++ src/test/scala/datastructures/TreeSpec.scala
@@ -1,21 +1,34 @@
 package datastructures
 
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.prop.Tables.Table
 import org.scalatest.{FlatSpec, Matchers}
 
 class TreeSpec extends FlatSpec with Matchers {
 
+  val fourLeaf = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
+
   "A Tree" should "count the number of nodes" in {
-
-    val tree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
-
-    Tree.size(tree) should be(7)
+    Tree.size(fourLeaf) should be(7)
   }
 
-  it should "know the maximum for tree of ints" in {
+  it should "know the maximum for tree of ints" in new {
+    Tree.maximum(fourLeaf) should be(4)
+  }
 
-    val tree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
+  it should "know the depth" in new PropertyChecks {
 
-    Tree.maxInt(tree) should be(4)
+    val combos =
+      Table(
+        ("tree", "depth"),
+        (fourLeaf, 2),
+        (Branch(Leaf(1), fourLeaf), 3),
+        (Branch(fourLeaf, Leaf(1)), 3)
+      )
+
+    forAll(combos) { (l: Tree[Int], depth: Int) =>
+      Tree.depth(l) should be(depth)
+    }
   }
 
 }
